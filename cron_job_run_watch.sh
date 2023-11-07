f8449f086528f8976f3d5c5dc6d5a09f47870f57
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-07 10:30:41 +0900

    Update project and run ID in cron job script.

--- cron_job_run_watch.sh
+++ cron_job_run_watch.sh
@@ -4,6 +4,6 @@ cd /Users/kazuki/Documents/TokyoTech/ylab/test/abci/wandb_watcher
 source .env/bin/activate
 
 python run_watcher.py --entity prj-jalm \
-  --project Llama-2-70B \
-  --run-id "g4qo4an6" \
+  --project Llama-2-7B \
+  --run-id "ffvh1mbx" \
   --interval-min 10
