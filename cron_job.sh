6980654d38a0a1bac7dc5b392e7a3deddc11d7fd
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-20 22:40:59 +0900

    release: v.1.0.0

--- cron_job.sh
+++ cron_job.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-cd /Users/kazuki/Documents/TokyoTech/ylab/test/abci/wandb_watcher
 source .env/bin/activate
 
 python watcher.py --entity prj-jalm \
+  --project Llama-2-70B \
+  --interval-min 10
+
+python watcher.py --entity prj-jalm \
   --project Llama-2-13B \
+  --interval-min 10
+
+python watcher.py --entity prj-jalm \
+  --project Llama-2-7B \
   --interval-min 10
--- cron_job_llama.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source .env/bin/activate
-
-python watcher_llama2_70b.py --entity prj-jalm \
-  --project Llama-2-7B \
-  --interval-min 10
